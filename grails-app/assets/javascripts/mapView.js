42650af389b2bdadf8116e499aa7eaff6cac04cf
melbsurfer <melbsurfer@gmail.com>
2017-01-25 21:16:49 -0500

    Added layerswitcher control to enable toggling between the basemaps.

--- grails-app/assets/javascripts/mapView.js
+++ grails-app/assets/javascripts/mapView.js
@@ -2,53 +2,55 @@
 //= require webjars/openlayers/3.20.1/ol.js
 //= require_self
 
-var MapView = (function ()
-{
-    function init()
-    {
-        var layers = [
-            // new ol.layer.Tile( {
-            //     source: new ol.source.TileWMS( {
-            //         url: 'https://ahocevar.com/geoserver/wms',
-            //         params: {
-            //             'LAYERS': 'ne:NE1_HR_LC_SR_W_DR'
-            //         }
-            //     } )
-            // } ),
-            new ol.layer.Tile( {
-                source: new ol.source.TileWMS( {
-                    url: '/wmsProxy',
-                    params: {
-                        'LAYERS': "o2-basemap-bright",
-                        'FORMAT': "image/jpeg"
-                    }
-                } )
-            } ),
-            new ol.layer.Tile( {
-                source: new ol.source.TileWMS( {
-                    url: '/wmsProxy',
-                    params: {
-                        'LAYERS': "o2-basemap-basic",
-                        'FORMAT': "image/jpeg"
-                    }
-                } )
-            } )
-        ];
+var MapView = (function() {
+    function init() {
 
-        var map = new ol.Map( {
-            controls: ol.control.defaults().extend( [
-                new ol.control.ScaleLine( {
+        var map = new ol.Map({
+            controls: ol.control.defaults().extend([
+                new ol.control.ScaleLine({
                     units: 'degrees'
-                } )
-            ] ),
-            layers: layers,
+                })
+            ]),
+            layers: [
+                new ol.layer.Group({
+                    'title': 'Base Maps',
+                    layers: [
+                      new ol.layer.Tile({
+                          title: 'OSM Bright',
+                          type: 'base',
+                          source: new ol.source.TileWMS({
+                              url: '/wmsProxy',
+                              params: {
+                                  'LAYERS': "o2-basemap-bright",
+                                  'FORMAT': "image/jpeg"
+                              }
+                          })
+                      }),
+                      new ol.layer.Tile({
+                          title: 'OSM Basic',
+                          type: 'base',
+                          source: new ol.source.TileWMS({
+                              url: '/wmsProxy',
+                              params: {
+                                  'LAYERS': "o2-basemap-basic",
+                                  'FORMAT': "image/jpeg"
+                              }
+                          })
+                      })
+                    ]
+                })
+            ],
             target: 'map',
-            view: new ol.View( {
+            view: new ol.View({
                 projection: 'EPSG:4326',
                 center: [0, 0],
                 zoom: 2
-            } )
-        } );
+            })
+        });
+
+        var layerSwitcher = new ol.control.LayerSwitcher();
+        map.addControl(layerSwitcher);
+
     }
 
     return {
--- grails-app/views/mapView/index.gsp
+++ grails-app/views/mapView/index.gsp
@@ -2,6 +2,7 @@
     <head>
         <meta name="layout" content="main"/>
         <asset:stylesheet src="mapView.css"/>
+        <asset:stylesheet src="ol3-layerswitcher.css"/>
     </head>
     <body>
         <div class="content">
@@ -9,6 +10,7 @@
             <div id="map"></div>
         </div>
         <asset:javascript src="mapView.js"/>
+        <asset:javascript src="ol3-layerswitcher.js"/>
         <asset:script>
         $(document).ready(function() {
             MapView.init();
